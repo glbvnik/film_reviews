4ecd0b8d1f7b50fda45b9c532e6ac97b049b3b3e
glbvnik <glbvnik@gmail.com>
2022-02-23 01:17:56 +0100

    update cookie domain

--- server/src/services/token.ts
+++ server/src/services/token.ts
@@ -24,19 +24,13 @@ export const TokenService = {
         res.cookie('accessToken', accessToken, {
             httpOnly: true,
             maxAge: 1000 * 60 * 15,
-            domain:
-                process.env.NODE_ENV === 'development'
-                    ? '.localhost'
-                    : '.domain.com',
+            domain: process.env.CLIENT_URL,
         })
 
         res.cookie('refreshToken', refreshToken, {
             httpOnly: true,
             maxAge: 1000 * 60 * 60 * 24 * 90,
-            domain:
-                process.env.NODE_ENV === 'development'
-                    ? '.localhost'
-                    : '.domain.com',
+            domain: process.env.CLIENT_URL,
         })
     },
     deleteCookie(res: Response) {
